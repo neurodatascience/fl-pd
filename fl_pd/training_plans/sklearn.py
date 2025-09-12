c23edfe6ef91d0093a41ef7dd73b8237622f50e2
Michelle Wang <tomichellewang@gmail.com>
2025-09-12 18:04:08 -0400

    do not use Adam optimizer!

--- fl_pd/training_plans/sklearn.py
+++ fl_pd/training_plans/sklearn.py
@@ -3,9 +3,7 @@ from fedbiomed.common.training_plans import FedSGDClassifier, FedSGDRegressor
 from fl_pd.training_plans.mixins import DataLoaderMixin, AdamOptimizerMixin
 
 
-class SklearnClassifierTrainingPlan(
-    DataLoaderMixin, AdamOptimizerMixin, FedSGDClassifier
-):
+class SklearnClassifierTrainingPlan(DataLoaderMixin, FedSGDClassifier):
     pass
 
 
